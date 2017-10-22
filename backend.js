3c22db933e5108c54284bd6e95af6f4b8e8f6709
unknown <idzer0lis@gmail.com>
2017-10-23 00:57:55 +0300

    adapted backend.js for heroku

--- backend.js
+++ backend.js
@@ -74,6 +74,12 @@ var vehicles = {
 //   res.sendFile(path.join(__dirname + '/dist/templates/index.html'));
 // });
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 app.get('/api/cars', function (req, res) {
   var responseJson = {};
   responseJson.discount_percentage = getRandomInt(0, 20);
